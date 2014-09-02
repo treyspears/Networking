465409ae642c8ba88a9310947793f9984853fc5c
Trey <stickman13@msn.com>
2014-09-02 13:24:36 -0500

    had to move something
    didn't compile

--- Voodoo/Game/Game.cpp
+++ Voodoo/Game/Game.cpp
@@ -46,9 +46,9 @@ int g_currentLogIndex = 0;
 #define BUFFER_LIMIT 512
 
 #define STARTING_SERVER_IP "127.0.0.1"
-#define STARTING_SERVER_PORT "5554"
+#define STARTING_SERVER_PORT "5553"
 
-const playerID PLAYER_ID( 255, 0, 0 );
+
 
  u_long BLOCKING = 0;
  u_long NON_BLOCKING = 1;
@@ -110,6 +110,8 @@ struct player
 	float currentInterpolatedTime;
 	float interpolationDuration;
 };
+
+const playerID PLAYER_ID( 255, 0, 0 );
 
 const Vector2f INITIAL_PLAYER_POSITION( 0.f, 0.f );
 const float INTERPOLATION_SCALE = 0.1f;
